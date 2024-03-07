b8754896e03f90a318f973884452a4843f4259c4
develop_yunju <ilove4903@naver.com>
2024-03-07 13:48:38 +0900

    SpringBoots_240307

--- src/main/java/com/example/sb/Users/UserController.java
+++ src/main/java/com/example/sb/Users/UserController.java
@@ -6,62 +6,99 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller	
+@Controller
 @RequestMapping("/user")
 public class UserController {
-	@Autowired private UserService userSvc;	// UserService 를 쓸 수 있다
-		@GetMapping("/list/{page}")
-		public String list(@PathVariable int page, Model model) {
-			List<User> list = userSvc.getUserList(page);
-			model.addAttribute("userList", list);
-			return "user/list";
-		}
-		
-		@GetMapping("/register")
-		public String register() {
-			return "user/register";
-		}
-		
-		@PostMapping("/register")
-		public String registerProc(String uid, String pwd, String pwd2, String uname, String email) {
-			if(userSvc.getUserByUid(uid) == null && pwd.equals(pwd2)) {
-				String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
-				User user = new User(uid, hashedPwd, uname, email);
-				userSvc.registerUser(user);
-			}
-			
-			return "redirect:/user/list/1";
-		}
-		
-		@GetMapping("/update")
-		public String update(String uid) { 
-			User user = userSvc.getUserByUid(uid);
-			return "user/update";
-			
-		}
-		@PostMapping("/update")
-		public String updateProc(String uid, String pwd, String pwd2, String uname, String email) {
-			User user = userSvc.getUserByUid(uid);
-			if(pwd.equals(pwd2)) {
-				String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
-				user.setPwd(hashedPwd);
-				user.setEmail(email);
-				user.setUname(uname);
-				userSvc.updateUser(user);
-				return "redirect:/user/list/1";
-		} else {
-			return "user/update";
-		}
-	}
-		
-		@PostMapping("/delete")
-		public String delete() {
-			return "user/delete";
-		}
+	@Autowired private UserService userSvc;
 
+	@GetMapping(value={"/list/{page}", "/list"})
+	public String list(@PathVariable(required=false) Integer page, Model model) {
+		page = (page == null) ? 1 : page;
+		List<User> list = userSvc.getUserList(page);
+		model.addAttribute("userList", list);
+		return "user/list";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "user/register";
+	}
+	
+	@PostMapping("/register")
+	public String registerProc(String uid, String pwd, String pwd2, String uname, String email) {
+		if (userSvc.getUserByUid(uid) == null && pwd.equals(pwd2)) {
+			String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
+			User user = new User(uid, hashedPwd, uname, email);
+			userSvc.registerUser(user);
+		}
+		return "redirect:/user/list/1";
+	}
+
+	@GetMapping("/update/{uid}")
+	public String update(@PathVariable String uid, Model model) {
+		User user = userSvc.getUserByUid(uid);
+		model.addAttribute("user", user);
+		return "user/update";
+	}
+	
+	@PostMapping("/update")
+	public String updateProc(String uid, String pwd, String pwd2, String uname, String email) {
+		User user = userSvc.getUserByUid(uid);
+		if (pwd != null && pwd.equals(pwd2)) {
+			String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
+			user.setPwd(hashedPwd);
+		}
+		user.setUname(uname);
+		user.setEmail(email);
+		userSvc.updateUser(user);
+		return "redirect:/user/list";
+	}
+	
+	@GetMapping("/delete/{uid}")
+	public String delete(@PathVariable String uid) {
+		userSvc.deleteUser(uid);
+		return "redirect:/user/list";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "user/login";
+	}
+	
+	@PostMapping("/login")
+	public String loginProc(String uid, String pwd, HttpSession session, Model model) {
+		String msg = null, url = null;
+		int result = userSvc.login(uid, pwd);
+		if (result == userSvc.CORRECT_LOGIN) {
+			User user = userSvc.getUserByUid(uid);
+			session.setAttribute("sessUid", uid);
+			session.setAttribute("sessUname", user.getUname());
+			msg = user.getUname() + "님 환영합니다.";
+			url = "/sb/user/list/1";
+		} else if (result == userSvc.WRONG_PASSWORD) {
+			msg = "패스워드가 틀립니다.";
+			url = "/sb/user/login";
+		} else {
+			msg = "아이디 입력이 잘못되었습니다.";
+			url = "/sb/user/login";
+		}
+		model.addAttribute("msg", msg);
+		model.addAttribute("url", url);
+		return "user/alertMsg";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/user/login";
+	}
+	
 }
--- src/main/java/com/example/sb/Users/UserServiceImpl.java
+++ src/main/java/com/example/sb/Users/UserServiceImpl.java
@@ -1,54 +1,56 @@
 package com.example.sb.users;
 
+import java.util.List;
+
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserDao uDao;
+	@Autowired
+	private UserDao uDao;
+	
+	@Override
+	public User getUserByUid(String uid) {
+		User user = uDao.getUser(uid);
+		return user;
+	}
 
-    @Override
-    public User getUserByUid(String uid) {
-        return uDao.getUser(uid);
-    }
+	@Override
+	public List<User> getUserList(int page) {
+		int offset = (page - 1) * COUNT_PER_PAGE;
+		return uDao.getUserList(COUNT_PER_PAGE, offset);
+	}
 
-    @Override
-    public List<User> getUserList(int page) {
-        int offset = (page - 1) * COUNT_PER_PAGE;
-        return uDao.getUserList(COUNT_PER_PAGE, offset);
-    }
+	@Override
+	public int getUserCount() {
+		return uDao.getUserCount();
+	}
 
-    @Override
-    public int getUserCount() {
-        return uDao.getUserCount();
-    }
+	@Override
+	public void registerUser(User user) {
+		uDao.insertUser(user);
+	}
 
-    @Override
-    public void registerUser(User user) {
-        uDao.insertUser(user);
-    }
+	@Override
+	public void updateUser(User user) {
+		uDao.updateUser(user);
+	}
 
-    @Override
-    public void updateUser(User user) {
-        uDao.updateUser(user);
-    }
+	@Override
+	public void deleteUser(String uid) {
+		uDao.deleteUser(uid);
+	}
 
-    @Override
-    public void deleteUser(String uid) {
-        uDao.deleteUser(uid);
-    }
+	@Override
+	public int login(String uid, String pwd) {
+		User user = uDao.getUser(uid);
+		if (user == null)
+			return USER_NOT_EXIST;
+		if (BCrypt.checkpw(pwd, user.getPwd()))
+			return CORRECT_LOGIN;
+		return WRONG_PASSWORD;
+	}
 
-    @Override
-    public int login(String uid, String pwd) {
-        User user = uDao.getUser(uid);
-        if (user == null)
-            return USER_NOT_EXIST;
-        if (BCrypt.checkpw(pwd, user.getPwd()))
-            return CORRECT_LOGIN;
-        return WRONG_PASSWORD;
-    }
 }
